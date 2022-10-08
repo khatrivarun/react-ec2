2e2a40299da67f33de4da005f8dfa328b2ff6567
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-10-08 18:50:55 +0530

    Beanstalk Update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React + Jenkins!
+          Learn React + Jenkins + Elastic Beanstalk!
         </a>
       </header>
     </div>
